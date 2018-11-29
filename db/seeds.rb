bfffc6ca9eb030bc283c5bb56d47be6abac82637
Vadim Tsvid <vadim.tsvid@gmail.com>
2018-11-29 19:11:47 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Event.create kind: 'opening',
-             starts_at: DateTime.parse('2014-08-04 09:30'),
-             ends_at: DateTime.parse('2014-08-04 12:30'),
-             weekly_recurring: true
+initial_date = DateTime.parse('2014-08-04 09:00')
 
-Event.create kind: 'appointment',
-             starts_at: DateTime.parse('2014-08-11 10:30'),
-             ends_at: DateTime.parse('2014-08-11 11:30')
+def recurring?
+  Event.recurring.count < 2 && rand(1..1000) == 1
+end
+
+10000.times do |i|
+  Event.create kind: 'opening',
+               starts_at: initial_date + [0, 30].sample.minutes,
+               ends_at: initial_date + [120, 180, 240].sample.minutes,
+               weekly_recurring: recurring?
+
+  Event.create kind: 'appointment',
+               starts_at: initial_date + [30, 60].sample.minutes,
+               ends_at: initial_date + [90, 120].sample.minutes
+
+  Event.create kind: 'opening',
+               starts_at: initial_date + [300, 330, 360].sample.minutes,
+               ends_at: initial_date + [480, 540, 720].sample.minutes,
+               weekly_recurring: recurring?
+
+  Event.create kind: 'appointment',
+               starts_at: initial_date + [390, 420].sample.minutes,
+               ends_at: initial_date + [450, 480].sample.minutes
+
+  initial_date += 1.day
+end
