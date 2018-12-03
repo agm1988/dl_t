102e6f3757c21789eba1df9d0a4dd8544f467182
v-tsvid <v-tsvid@users.noreply.github.com>
2018-12-03 11:57:09 +0200

    Revert "Calc slots with postgres"

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,77 +1,85 @@
 class Event < ApplicationRecord
   SCAN_DAYS = 7
+  INTERVAL = 30.minutes
+
+  scope :openings, -> { where(kind: 'opening') }
+  scope :appointments, -> { where(kind: 'appointment') }
+  scope :recurring, -> { openings.where(weekly_recurring: true) }
+  scope :not_recurring, -> { openings.where(weekly_recurring: false) }
+  scope :asc_by_starts_at, -> { order('starts_at asc') }
+
+  scope :openings_by_date, lambda { |date|
+    not_recurring
+      .where('starts_at >= ? AND starts_at < ?',
+             date,
+             date + 1.day)
+      .or(recurring
+            .where('starts_at < ? AND EXTRACT(DOW FROM starts_at) = ?',
+                   date + 1.day,
+                   date.wday))
+      .asc_by_starts_at
+  }
+
+  scope :appointments_by_date, lambda { |date|
+    appointments.where('starts_at >= ? AND ends_at <= ?',
+                       date.beginning_of_day,
+                       date.end_of_day + (SCAN_DAYS-1).days)
+  }
 
   class << self
     def availabilities(start_date)
       result = []
       current_date = start_date
+      appointments = appointments_by_date(current_date)
+                       .pluck(:starts_at, :ends_at)
       SCAN_DAYS.times do |i|
-        app_slots = appointment_slots(current_date)
-        op_slots = opening_slots(current_date)
-        slots = (op_slots - app_slots).sort.map! do |s|
-          s[0, 5].delete_prefix('0')
-        end
-        result << { date: current_date, slots: slots}
-        current_date = start_date + (i + 1).days
+        result << { date: current_date,
+                    slots: slots(current_date, appointments) }
+        current_date = start_date + (i+1).days
       end
       result
     end
 
     private
 
-    def appointment_slots(date)
-      connection
-        .select_all(
-          sanitize_sql_array(
-            [select_str << appointment_where_clause,
-             'appointment',
-             date.beginning_of_day,
-             date.end_of_day + 1.day]))
-        .rows
-        .flatten
-        .uniq
+    def slots(current_date, appointments)
+      openings = openings_by_date(current_date)
+                   .pluck(:starts_at, :ends_at)
+      return [] unless openings.length > 0
+      openings.map! do |o|
+        slots_for_opening(o, appointments, current_date)
+      end
+      openings.flatten.uniq
     end
 
-    def opening_slots(date)
-      connection
-        .select_all(
-          sanitize_sql_array(
-            [select_str << opening_where_clause,
-             'opening',
-             false,
-             date,
-             date + 1.day,
-             true,
-             date + 1.day,
-             date.wday]))
-        .rows
-        .flatten
-        .uniq
+    def slots_for_opening(opening, appointments, date)
+      result = []
+      starts_at = opening[0]
+      loop do
+        break if last_slot?(opening, starts_at + INTERVAL)
+        unless booked?(appointments, date, starts_at)
+          result << starts_at.strftime('%-H:%M')
+        end
+        starts_at += INTERVAL
+      end
+      result
     end
 
-    def select_str
-      "SELECT DISTINCT (
-           generate_series (
-               events.starts_at,
-               (events.ends_at - '30 minutes'::interval),
-               '30 minutes'::interval
-           )
-       )::time as slots
-       FROM events "
+    def last_slot?(opening, starts_at)
+      starts_at > opening[1] ||
+        starts_at > opening[0].tomorrow.beginning_of_day
     end
 
-    def appointment_where_clause
-      "WHERE kind = ? AND (starts_at >= ? AND ends_at <= ?)"
-    end
-
-    def opening_where_clause
-      "WHERE kind = ? AND (
-           weekly_recurring = ? AND (
-               starts_at >= ? AND starts_at < ?
-           ) OR weekly_recurring = ? AND (
-               starts_at < ? AND EXTRACT(DOW FROM starts_at) = ?
-           )
-       )"
+    def booked?(appointments, date, time)
+      return false unless appointments.length > 0
+      date_str = date.strftime('%F')
+      time_str = time.strftime('%H:%M')
+      datetime = DateTime.new(date_str[0, 4].to_i,
+                              date_str[5, 2].to_i,
+                              date_str[8, 2].to_i,
+                              time_str[0, 2].to_i,
+                              time_str[3,2].to_i)
+      appointments.count { |x| x[0] <= datetime && x[1] > datetime } > 0
     end
   end
 end
